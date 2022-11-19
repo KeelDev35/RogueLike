18566e19fa4da127c5a3464aa456528d136de754
KeelDev35 <eluard.k@gmail.com>
2022-11-19 19:35:54 +0100

    first

---
+++ Tiled/Accessoires3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Accessoires" tilewidth="16" tileheight="16" tilecount="9" columns="3">
+ <image source="GB PROJECT/Graphics/PngFolder/TileMap/Accessories/Global/Accessoires.png" width="48" height="48"/>
+</tileset>
